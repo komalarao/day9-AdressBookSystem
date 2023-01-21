33361a72df981b451baec6fc04a394bc742f5ae2
komalarao <manoharvakamullu1999@gmail.com>
2023-01-21 00:23:46 -0800

    Refactor: program for add new contact and to edit existing contact

--- src/com/bridgelabz/adressBookSystem/AddressBook.java
+++ src/com/bridgelabz/adressBookSystem/AddressBook.java
@@ -1,13 +1,18 @@
 package com.bridgelabz.adressBookSystem;
 import java.util.Scanner;
+import java.util.ArrayList;
+
 public class AddressBook {
 	Scanner scanner = new Scanner(System.in);
-
-	ContactPerson contact = new ContactPerson(); // create a ContactPerson object
-
-	// Add contacts
-
+	ContactPerson contact = new ContactPerson();
+	ArrayList<ContactPerson> contactsArrayList = new ArrayList<ContactPerson>();
+	
+	/*
+	 * Adding contacts
+	 */
 	public void addContact() {
+		ContactPerson contact = new ContactPerson();
+		
 		System.out.println("Enter your FirstName: ");
 		contact.setFirstName(scanner.next());
 		System.out.println("Enter your LastName: ");
@@ -24,10 +29,57 @@ public class AddressBook {
 		contact.setZip(scanner.nextInt());
 		System.out.println("Enter your Mobile Number: ");
 		contact.setMobile(scanner.nextLong());
+		
+		contactsArrayList.add(contact);
 	}
-
+	/*
+	 * Print out on all contacts using method contact class
+	 */
 	public void showContacts() {
 		System.out.println(contact.toString());
+		for (int i = 0; i < contactsArrayList.size(); i++) {
+			ContactPerson contacts = contactsArrayList.get(i);
+			System.out.println(contacts.toString());
+		}
+	}
+	
+	public void editContact() {
+		/*
+		 * Ability to edit existing contact person using their
+		 */
+		System.out.println("Enter the First Name to Edit : ");
+		String FirstName = (scanner.next());
+		boolean IsAvaible = false;
+
+		for (ContactPerson contact : contactsArrayList) {
+			if (contact.getFirstName().equals(FirstName)) {
+				IsAvaible = true;
+
+				System.out.println("Enter your FirstName: ");
+				contact.setFirstName(scanner.next());
+				System.out.println("Enter your LastName: ");
+				contact.setLastName(scanner.next());
+				System.out.println("Enter your Email: ");
+				contact.setEmail(scanner.next());
+				System.out.println("Enter your Address: ");
+				contact.setAddress(scanner.next());
+				System.out.println("Enter your City: ");
+				contact.setCity(scanner.next());
+				System.out.println("Enter your State: ");
+				contact.setState(scanner.next());
+				System.out.println("Enter your Zipcode: ");
+				contact.setZip(scanner.nextInt());
+				System.out.println("Enter your Mobile Number: ");
+				contact.setMobile(scanner.nextLong());
+			}
+		}
+		/*
+		 * If the condition false to have printed conatct doesnt exist
+		 */
+		if (IsAvaible == false) {
+			System.out.println("Contact Doesn't exist.");
+		}
+	
 	}
 
 }
