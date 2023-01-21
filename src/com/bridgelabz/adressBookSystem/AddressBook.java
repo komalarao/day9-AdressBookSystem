d857e578b9c96fc1e3448944911df4498d27c0ba
komalarao <manoharvakamullu1999@gmail.com>
2023-01-20 22:13:13 -0800

    added: address book program to create contact

---
+++ src/com/bridgelabz/adressBookSystem/AddressBook.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.adressBookSystem;
+
+public class AddressBook extends Contact{
+
+	public static void main(String[] args) {
+		AddressBook adressBook = new AddressBook();
+		adressBook.createContact();
+		adressBook.address();
+
+	}
+}
